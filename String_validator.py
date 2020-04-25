40bf286aed97ee6320bd9e85d6a17202cf8cfe06
yiliu <louise.yl7603@gmail.com>
2020-04-25 20:20:23 +0200

    Add String_validator.py

---
+++ String_validator.py
@@ -0,0 +1,108 @@
+# Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+#
+# str.isalnum()
+# This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+#
+# >>> print 'ab123'.isalnum()
+# True
+# >>> print 'ab123#'.isalnum()
+# False
+# str.isalpha()
+# This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+#
+# >>> print 'abcD'.isalpha()
+# True
+# >>> print 'abcd1'.isalpha()
+# False
+# str.isdigit()
+# This method checks if all the characters of a string are digits (0-9).
+#
+# >>> print '1234'.isdigit()
+# True
+# >>> print '123edsd'.isdigit()
+# False
+# str.islower()
+# This method checks if all the characters of a string are lowercase characters (a-z).
+#
+# >>> print 'abcd123#'.islower()
+# True
+# >>> print 'Abcd123#'.islower()
+# False
+# str.isupper()
+# This method checks if all the characters of a string are uppercase characters (A-Z).
+#
+# >>> print 'ABCD123#'.isupper()
+# True
+# >>> print 'Abcd123#'.isupper()
+# False
+# Task
+#
+# You are given a string .
+# Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+#
+# Input Format
+#
+# A single line containing a string .
+#
+# Constraints
+#
+#
+# Output Format
+#
+# In the first line, print True if  has any alphanumeric characters. Otherwise, print False.
+# In the second line, print True if  has any alphabetical characters. Otherwise, print False.
+# In the third line, print True if  has any digits. Otherwise, print False.
+# In the fourth line, print True if  has any lowercase characters. Otherwise, print False.
+# In the fifth line, print True if  has any uppercase characters. Otherwise, print False.
+#
+# Sample Input
+#
+# qA2
+# Sample Output
+#
+# True
+# True
+# True
+# True
+# True
+
+
+if __name__ == '__main__':
+    s = input()
+for test in ('isalnum', 'isalpha', 'isdigit', 'islower', 'isupper'):
+        print(any(eval("c." + test + "()") for c in s))
+
+# if __name__ == '__main__':
+#     s = input()
+#     n1, n2, n3, n4, n5 = 0, 0, 0, 0, 0
+#     for i in range(len(s)):
+#         if s[i].isalnum():
+#             n1 += 1
+#         if s[i].isalpha():
+#             n2 += 1
+#         if s[i].isdigit():
+#             n3 += 1
+#         if s[i].islower():
+#             n4 += 1
+#         if s[i].isupper():
+#             n5 += 1
+#     n = [n1, n2, n3, n4, n5]
+#     for j in n:
+#         if n != 0:
+#             print('True')
+#         else:
+#             print('False')
+#
+# if __name__ == '__main__':
+#     s = input()
+#     cmds = ['.isalnum()', '.isalpha()', '.isdigit()', '.islower()', '.isupper()']
+#     for cmd in cmds:
+#         n = 0
+#         for i in range(len(s)):
+#             s2 = "'" + s[i] + "'"
+#             if eval(s2 + cmd):
+#                 n += 1
+#         if n != 0:
+#             print('True')
+#         else:
+#             print('Flase')
